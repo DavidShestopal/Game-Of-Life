3e3c55d7c4624a122a8f715ea5864ef6a6786057
David Shestopal <dshestopal@yahoo.com>
2020-07-28 09:35:02 -0700

    logic added and working

---
+++ game-of-life/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import GameofLife from './GameOfLife';
+
+function App() {
+  return (
+    <div>
+      <GameofLife />
+    </div>
+  );
+}
+
+export default App;
